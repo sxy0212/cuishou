31efb5bdd61624f70b698a998002cc72f32bf4f8
CN-20140101LBOT\Administrator <756441967@qq.com>
2018-12-08 11:22:48 +0800

    change

--- src/views/Left.vue
+++ src/views/Left.vue
@@ -1,56 +1,24 @@
 <template>
-     <el-menu
-        default-active="1" :unique-opened="open" @open="handleOpen"
-          @close="handleClose"
-          @select="handleSelect" router
-        class="el-menu-vertical-demo" style="height:100%" background-color="#333744" text-color="#fff"  active-text-color="#ffd04b" >
-          <el-menu-item :index="item.path" v-for="(item,index) in menus" v-if="item.child.length==0" :key="index">
-            <i :class="item.icon"></i>
-            <span slot="title">{{item.name}}</span>
-          </el-menu-item>
-          <el-submenu :index="item.id" v-for="(item,index) in menus" v-if="item.child.length!=0" :key="index">
-            <template slot="title">
-              <i :class="item.icon"></i>
-              <span>{{item.name}}</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item :index="item.path" v-for="(item,index) in item.child" :key="index">{{item.name}}</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu> 
-          <!--<el-menu-item index="/caseManagement">
-            <i class="fa fa-home"></i>
-            <span slot="title">案件管理</span>
-          </el-menu-item> 
-          <el-menu-item index="/dataManagement">
-            <i class="fa fa-home"></i>
-            <span slot="title">资料管理</span>
-          </el-menu-item> 
-          <el-menu-item index="/smsManagement">
-            <i class="fa fa-home"></i>
-            <span slot="title">短信管理</span>
-          </el-menu-item> 
-          <el-menu-item index="/speechScheme">
-            <i class="fa fa-home"></i>
-            <span slot="title">话术方案</span>
-          </el-menu-item> 
-           <el-menu-item index="/privilegeManagement">
-            <i class="fa fa-home"></i>
-            <span slot="title">权限管理</span>
-          </el-menu-item> 
-          <el-submenu index="2">
-            <template slot="title">
-              <i class="fa fa-chain"></i>
-              <span>系统设置</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="/regionalConfiguration">催收区域配置</el-menu-item>
-              <el-menu-item index="/clientConfiguration" >委托方配置</el-menu-item>
-              <el-menu-item index="/caseConfiguration">案件类型配置</el-menu-item>
-              <el-menu-item index="/accountManagement">账号管理</el-menu-item>
-              <el-menu-item index="/templateManagement">模板管理</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>  -->    
-      </el-menu>       
+    <el-menu
+      default-active="1" :unique-opened="open" @open="handleOpen"
+      @close="handleClose"
+      @select="handleSelect" router
+      class="el-menu-vertical-demo" 
+      style="height:100%" background-color="#333744" text-color="#fff"  active-text-color="#ffd04b" >
+      <el-menu-item :index="item.path" v-for="(item,index) in menus" v-if="item.child.length==0" :key="index">
+        <i :class="item.icon"></i>
+        <span slot="title">{{item.name}}</span>
+      </el-menu-item>
+      <el-submenu :index="item.id" v-for="(item,index) in menus" v-if="item.child.length!=0" :key="index">
+        <template slot="title">
+          <i :class="item.icon"></i>
+          <span>{{item.name}}</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item :index="item.path" v-for="(item,index) in item.child" :key="index">{{item.name}}</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu> 
+    </el-menu>       
 </template>
 <script type="text/ecmascript-6">
 import  { axiosRequest } from '@/assets/js/Yt.js'
