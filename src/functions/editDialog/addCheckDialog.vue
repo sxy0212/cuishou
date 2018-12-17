e7e60fd8ed0e4a9a61c96cfc891068d237e1e9b4
CN-20140101LBOT\Administrator <756441967@qq.com>
2018-12-17 12:02:53 +0800

    change

--- src/functions/editDialog/addCheckDialog.vue
+++ src/functions/editDialog/addCheckDialog.vue
@@ -76,31 +76,18 @@ export default {
             this.$emit('download')
         },
         playOne(path){
-            
-            // if( this.$refs.audio.paused ){//自己暂停,自己播放
-            //     this.$refs.audio.src = '@/recordings/2018-12-14/20181214122815_6900011_13466910508.wav'
-            //     this.$refs.audio.play()
-            // }else{//自己播放，后自己暂停
-            //     this.$refs.audio.pause()
-            // }
-            console.log(this.$refs.audio.play,this.$refs.audio.paused,this.$refs.audio.pause )
-            if( this.$refs.audio.paused ){//自己播放，自己暂停
-                this.$refs.audio.src = '/static/sound/20181214122815_6900011_13466910508.wav'
-                // this.$refs.audio.play()
+            if( !this.$refs.audioPlay.paused ){//其他播放,其他暂停
+                this.$refs.audioPlay.pause()
+            }
+            if( this.$refs.audio.paused ){//自己暂停，自己播放
+               this.$refs.audio.src = path
             }
         },
         textListen(path){//单句录音播放
             this.$refs.audioPlay.src = path
-            if( this.$refs.audioPlay.paused ){//自己暂停
-                this.$refs.audioPlay.src = path
-                this.$refs.audioPlay.play()
-            }else{//自己播放，后自己暂停
-                this.$refs.audioPlay.pause()
-            }
             if( !this.$refs.audio.paused ){//其他播放，其他暂停
                 this.$refs.audio.pause()
             }
-            
         }
     }
 }
