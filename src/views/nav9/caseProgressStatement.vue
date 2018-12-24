e4d146394e92154cec0ea9601f238f32e227b9a8
sxy <1043382831@qq.com>
2018-12-24 18:13:31 +0800

    统计报表

--- src/views/nav9/caseProgressStatement.vue
+++ src/views/nav9/caseProgressStatement.vue
@@ -1,18 +1,84 @@
 <template>
-    <div>
-        <div>
-            <el-button type="primary" @click="saveFn" >电催员点翠月度统计</el-button>
-            <el-button type="primary" @click="saveFn" >电催还款率单月统计</el-button>
-            <el-button type="primary" @click="saveFn" >电催还款日度统计</el-button>
-            <el-button type="primary" @click="saveFn" >电催还款月度统计</el-button>
+    <section>
+    <div class="CenterMain  MainHp">
+      <div class="TopForm">
+        <div class="seachForm">
+          <el-form ref="form" :model="form" label-width="75px" :inline="true" >
+            <el-form-item label="催收员:">
+              <el-select v-model="form.status" placeholder="请选择状态" :style="width">
+                <el-option label="全部" value=""></el-option>
+                <el-option label="发送成功" value="1"></el-option>
+                <el-option label="发送失败" value="0"></el-option>  
+              </el-select>
+            </el-form-item>
+             <el-form-item label="催收区域:">
+              <el-select v-model="form.status" placeholder="请选择状态" :style="width">
+                <el-option label="全部" value=""></el-option>
+                <el-option label="发送成功" value="1"></el-option>
+                <el-option label="发送失败" value="0"></el-option>  
+              </el-select>
+            </el-form-item>
+             <el-form-item label="委托方:">
+              <el-select v-model="form.status" placeholder="请选择状态" :style="width">
+                <el-option label="全部" value=""></el-option>
+                <el-option label="发送成功" value="1"></el-option>
+                <el-option label="发送失败" value="0"></el-option>  
+              </el-select>
+            </el-form-item>
+            <el-form-item label="还款情况:">
+              <el-select v-model="form.status" placeholder="请选择状态" :style="width">
+                <el-option label="全部" value=""></el-option>
+                <el-option label="发送成功" value="1"></el-option>
+                <el-option label="发送失败" value="0"></el-option>  
+              </el-select>
+            </el-form-item>
+             <el-form-item label="批次:">
+              <el-select v-model="form.status" placeholder="请选择状态" :style="width">
+                <el-option label="全部" value=""></el-option>
+                <el-option label="发送成功" value="1"></el-option>
+                <el-option label="发送失败" value="0"></el-option>  
+              </el-select>
+            </el-form-item>
+            <el-form-item label="案件类型:">
+              <el-select v-model="form.status" placeholder="请选择状态" :style="width">
+                <el-option label="全部" value=""></el-option>
+                <el-option label="发送成功" value="1"></el-option>
+                <el-option label="发送失败" value="0"></el-option>  
+              </el-select>
+            </el-form-item>
+            <el-button type="primary" @click="init(1)">立即搜索</el-button>
+            <el-button type="primary" @click="init(1)">导出报表</el-button>
+          </el-form>
         </div>
-        <div>
-            <el-button type="primary" @click="saveFn" >委托方还款日度统计</el-button>
-            <el-button type="primary" @click="saveFn" >案件还款明细表</el-button>
-            <el-button type="primary" @click="saveFn" >案件跟进度统计</el-button>
-            <el-button type="primary" @click="saveFn" >批次状态分类统计</el-button>
+        <div class="TableList">
+          <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" border stripe style="width: 100%" :height="total>0?500:100">
+            <el-table-column type="index" :index="index" label="序号" width="50"></el-table-column>
+            <el-table-column prop="mobile" label="案件催收员"> </el-table-column>
+            <el-table-column prop="content" label="从未跟进"></el-table-column>
+            <el-table-column prop="user_id" label="1-3天未跟进"></el-table-column> 
+            <el-table-column prop="user_id" label="7-15天未跟进"></el-table-column> 
+            <el-table-column prop="user_id" label="16-30天未跟进"></el-table-column> 
+            <el-table-column prop="user_id" label="31-45天未跟进"></el-table-column> 
+            <el-table-column prop="user_id" label="46-60天未跟进"></el-table-column> 
+            <el-table-column prop="user_id" label="60天以上未跟进"></el-table-column> 
+          </el-table>
         </div>
+        <div class="pagination" v-show="!!total">
+            <div class="block"> 
+                <el-pagination
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="currentPage"
+                    :page-sizes="[10, 15, 20, 25]"
+                    :page-size="form.page_size"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total="total">
+                </el-pagination>
+            </div>
+        </div>
+      </div>
     </div>
+  </section>
 </template>
 <script>
 
@@ -22,13 +88,60 @@ export default {
     },
     data(){
         return{
-            
+         total:0,
+        currentPage:1,
+        form:{
+          page:1,
+          page_size:10,
+          start_time:"",
+          end_time:""
+        },
+        tableData:[],
+        sms_count:0
         }
     },
+    activated(){
+      this.init(0)
+    },
     methods:{
-        saveFn(){
-            
+        index(val){
+        return  (this.form.page-1)*this.form.page_size+val+1
+      },
+      //数据列表
+      init(num){
+        const data = this.form
+        if(num == 0){
+          data.page = this.form.page
+          data.page_size = this.form.page_size
+        }else if(num == 1){
+          data.page = 1;
+          data.page_size = 10
         }
+        const conf = {
+          url:"/api/api_backend.php?r=sms-log/sms-log",
+          data:data,
+          success:(data)=>{
+            if( data.statusCode == 1 ){
+              this.tableData = data.info.info
+              this.total = parseInt(data.info.total)
+              this.sms_count = parseInt(data.info.sms_count)
+            }else{
+              this.tableData = []
+              this.total = 0
+              this.sms_count = 0
+            }
+          }
+        }
+        axiosRequest(conf)
+      },
+         handleSizeChange(val) {
+        this.form.page_size = val
+        this.sendRecord(0)
+      },
+      handleCurrentChange(val) {
+        this.form.page = val
+        this.sendRecord(0)
+      },
     }
 }
 </script>
--- src/views/nav9/clientRepaymentStatement.vue
+++ src/views/nav9/clientRepaymentStatement.vue
@@ -1,28 +1,126 @@
 <template>
-    <div>
-        <div>
-            <el-button type="primary" @click="saveFn" >电催员点翠月度统计</el-button>
-            <el-button type="primary" @click="saveFn" >电催还款率单月统计</el-button>
-            <el-button type="primary" @click="saveFn" >电催还款日度统计</el-button>
-            <el-button type="primary" @click="saveFn" >电催还款月度统计</el-button>
-        </div>
-        <div>
-            <el-button type="primary" @click="saveFn" >委托方还款日度统计</el-button>
-            <el-button type="primary" @click="saveFn" >案件还款明细表</el-button>
-            <el-button type="primary" @click="saveFn" >案件跟进度统计</el-button>
-            <el-button type="primary" @click="saveFn" >批次状态分类统计</el-button>
-        </div>
-    </div>
+    <section>
+      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+		<el-form-item label="催收区域:">
+			<el-select v-model="formInline.collection_area" placeholder="请选择催收区域">
+				<el-option label="西安" value="1"></el-option>
+				<el-option label="北京" value="2"></el-option>
+				<el-option label="上海" value="3"></el-option>
+				<el-option label="合肥" value="4"></el-option>
+				<el-option label="安徽" value="5"></el-option>
+			</el-select>
+		</el-form-item>
+		<el-form-item label="委托方:">
+			<el-select v-model="formInline.client" placeholder="请选择委托方">
+				<el-option label="中国银行" value="1"></el-option>
+				<el-option label="中国人寿" value="2"></el-option>
+			</el-select>
+		</el-form-item>
+		<el-form-item label="还款日期:">
+			<el-date-picker
+                v-model="formInline.date"
+                type="date"
+                placeholder="选择日期">
+            </el-date-picker>
+		</el-form-item>
+		<el-form-item>
+            <el-button type="primary"  plain >开始统计</el-button>
+            <el-button type="primary"  plain >导出报表</el-button>
+        </el-form-item>
+	</el-form>
+    <el-table :data="tableData6" border show-summary style="width: 100%">
+        <el-table-column prop="id" label="委托方" width="110"></el-table-column>
+        <el-table-column prop="name" label="催收区域" width="100"></el-table-column>
+        <el-table-column prop="amount1" label="2018-12-01"></el-table-column>
+        <el-table-column prop="amount2" label="2018-12-02"></el-table-column>
+        <el-table-column prop="amount3" label="2018-12-03"></el-table-column>
+        <el-table-column prop="amount4" label="2018-12-04"></el-table-column>
+        <el-table-column prop="amount5" label="2018-12-05"></el-table-column>
+        <el-table-column prop="amount6" label="2018-12-06"></el-table-column>
+        <el-table-column prop="amount7" label="2018-12-07"></el-table-column>
+        <el-table-column prop="amount8" label="2018-12-08"></el-table-column>
+        <el-table-column prop="amount9" label="2018-12-09"></el-table-column>
+        <el-table-column prop="amount10" label="2018-12-10"></el-table-column>
+    </el-table>
+    </section>
+   
 </template>
 <script>
-
+import {axiosRequest,message} from '@/assets/js/Yt.js'
 export default {
-    components:{
-        
-    },
     data(){
         return{
-            
+            formInline:{
+                collection_area:"",
+                client:"",
+                date:""
+            },
+            tableData6: [{
+                id: '51信用卡',
+                name: '合肥',
+                amount1: 234,
+                amount2: 3.2,
+                amount3: 95,
+                amount4: 28,
+                amount5: 15,
+                amount6: 11,
+                amount7: 9,
+                amount8: 12,
+                amount9: 19,
+                amount10: 17,
+                }, {
+                id: '51信用卡',
+                name: '合肥',
+                amount1: 234,
+                amount2: 3.2,
+                amount3: 16,
+                amount4: 12,
+                amount5: 11,
+                amount6: 16,
+                amount7: 19,
+                amount8: 25,
+                amount9: 29,
+                amount10: 36,
+                }, {
+                id: '51信用卡',
+                name: '安徽',
+                amount1: 23,
+                amount2: 3.2,
+                amount3: 10,
+                amount4: 19,
+                amount5: 10,
+                amount6: 12,
+                amount7: 99,
+                amount8: 12,
+                amount9: 10,
+                amount10: 17,
+                }, {
+                id: '51信用卡',
+                name: '北京',
+                amount1: '234',
+                amount2: '3.2',
+                amount3: 10,
+                amount4: 10,
+                amount5: 10,
+                amount6: 10,
+                amount7: 10,
+                amount8: 10,
+                amount9: 10,
+                amount10: 10,
+                }, {
+                id: '51信用卡',
+                name: '上海',
+                amount1: 234,
+                amount2: 3.2,
+                amount3: 3,
+                amount4: 11,
+                amount5: 12,
+                amount6: 94,
+                amount7: 12,
+                amount8: 5,
+                amount9: 9,
+                amount10: 3,
+            }]
         }
     },
     methods:{
