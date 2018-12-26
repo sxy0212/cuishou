bbf3781675d82eac6c11192989ebdc523afd7c8e
sxy <1043382831@qq.com>
2018-12-26 13:47:08 +0800

    案件跟进统计

--- src/views/nav9/caseProgressStatement.vue
+++ src/views/nav9/caseProgressStatement.vue
@@ -5,76 +5,58 @@
         <div class="seachForm">
           <el-form ref="form" :model="form" label-width="75px" :inline="true" >
             <el-form-item label="催收员:">
-              <el-select v-model="form.status" placeholder="请选择状态" :style="width">
-                <el-option label="全部" value=""></el-option>
-                <el-option label="发送成功" value="1"></el-option>
-                <el-option label="发送失败" value="0"></el-option>  
+              <el-select v-model="staff_id" placeholder="请选择" multiple :style="width">
+                <el-option :label="item.true_name" :value="item.id" v-for="(item,index) in staff_list" :key="index"></el-option>
               </el-select>
             </el-form-item>
              <el-form-item label="催收区域:">
-              <el-select v-model="form.status" placeholder="请选择状态" :style="width">
-                <el-option label="全部" value=""></el-option>
-                <el-option label="发送成功" value="1"></el-option>
-                <el-option label="发送失败" value="0"></el-option>  
+              <el-select v-model="form.area_id" placeholder="请选择" :style="width">
+                <el-option label="请选择" value=""></el-option>
+                <el-option :label="item.name" :value="item.id" v-for="(item,index) in area_list" :key="index"></el-option>                
               </el-select>
             </el-form-item>
              <el-form-item label="委托方:">
-              <el-select v-model="form.status" placeholder="请选择状态" :style="width">
-                <el-option label="全部" value=""></el-option>
-                <el-option label="发送成功" value="1"></el-option>
-                <el-option label="发送失败" value="0"></el-option>  
+              <el-select v-model="form.client_id" placeholder="请选择" :style="width">
+                <el-option label="请选择" value=""></el-option>
+                <el-option :label="item.name" :value="item.id" v-for="(item,index) in client_list" :key="index"></el-option>                                
               </el-select>
             </el-form-item>
             <el-form-item label="还款情况:">
-              <el-select v-model="form.status" placeholder="请选择状态" :style="width">
-                <el-option label="全部" value=""></el-option>
-                <el-option label="发送成功" value="1"></el-option>
-                <el-option label="发送失败" value="0"></el-option>  
+              <el-select v-model="form.paid" placeholder="请选择" :style="width">
+                <el-option label="请选择" value=""></el-option>
+                <el-option label="已还" value="1"></el-option>
+                <el-option label="未还" value="0"></el-option>  
               </el-select>
             </el-form-item>
              <el-form-item label="批次:">
-              <el-select v-model="form.status" placeholder="请选择状态" :style="width">
-                <el-option label="全部" value=""></el-option>
-                <el-option label="发送成功" value="1"></el-option>
-                <el-option label="发送失败" value="0"></el-option>  
+              <el-select v-model="form.batch_id" placeholder="请选择" :style="width">
+                <el-option label="请选择" value=""></el-option>
+                <el-option :label="item.batch_name" :value="item.id" v-for="(item,index) in batch_list" :key="index"></el-option>                               
               </el-select>
             </el-form-item>
             <el-form-item label="案件类型:">
-              <el-select v-model="form.status" placeholder="请选择状态" :style="width">
-                <el-option label="全部" value=""></el-option>
-                <el-option label="发送成功" value="1"></el-option>
-                <el-option label="发送失败" value="0"></el-option>  
+              <el-select v-model="form.type_id" placeholder="请选择" :style="width">
+                <el-option label="请选择" value=""></el-option>
+                <el-option :label="item.name" :value="item.id" v-for="(item,index) in type_list" :key="index"></el-option>                                              
               </el-select>
             </el-form-item>
-            <el-button type="primary" @click="init(1)">立即搜索</el-button>
+            <el-button type="primary" @click="init()">开始统计</el-button>
             <el-button type="primary" @click="init(1)">导出报表</el-button>
           </el-form>
         </div>
-        <div class="TableList">
-          <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" border stripe style="width: 100%" :height="total>0?500:100">
-            <el-table-column type="index" :index="index" label="序号" width="50"></el-table-column>
-            <el-table-column prop="mobile" label="案件催收员"> </el-table-column>
-            <el-table-column prop="content" label="从未跟进"></el-table-column>
-            <el-table-column prop="user_id" label="1-3天未跟进"></el-table-column> 
-            <el-table-column prop="user_id" label="7-15天未跟进"></el-table-column> 
-            <el-table-column prop="user_id" label="16-30天未跟进"></el-table-column> 
-            <el-table-column prop="user_id" label="31-45天未跟进"></el-table-column> 
-            <el-table-column prop="user_id" label="46-60天未跟进"></el-table-column> 
-            <el-table-column prop="user_id" label="60天以上未跟进"></el-table-column> 
+        <div>
+          <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" border stripe style="width: 100%">
+            <el-table-column type="index" label="序号" width="50"></el-table-column>
+            <el-table-column prop="name" label="案件催收员"> </el-table-column>
+            <el-table-column prop="never" label="从未跟进"></el-table-column>
+            <el-table-column prop="1to3" label="1-3天未跟进"></el-table-column> 
+            <el-table-column prop="4to6" label="4-6天未跟进"></el-table-column> 
+            <el-table-column prop="7to15" label="7-15天未跟进"></el-table-column> 
+            <el-table-column prop="16to30" label="16-30天未跟进"></el-table-column> 
+            <el-table-column prop="31to45" label="31-45天未跟进"></el-table-column> 
+            <el-table-column prop="46to60" label="46-60天未跟进"></el-table-column> 
+            <el-table-column prop="60tomax" label="60天以上未跟进"></el-table-column> 
           </el-table>
-        </div>
-        <div class="pagination" v-show="!!total">
-            <div class="block"> 
-                <el-pagination
-                    @size-change="handleSizeChange"
-                    @current-change="handleCurrentChange"
-                    :current-page="currentPage"
-                    :page-sizes="[10, 15, 20, 25]"
-                    :page-size="form.page_size"
-                    layout="total, sizes, prev, pager, next, jumper"
-                    :total="total">
-                </el-pagination>
-            </div>
         </div>
       </div>
     </div>
@@ -83,65 +65,67 @@
 <script>
 import {axiosRequest,formatDate} from '@/assets/js/Yt.js'
 export default {
-    components:{
-        
-    },
     data(){
         return{
-         total:0,
-        currentPage:1,
-        form:{
-          page:1,
-          page_size:10,
-          start_time:"",
-          end_time:""
-        },
-        tableData:[],
-        sms_count:0
+            staff_list:[],// |  | 催收员 |
+            area_list:[],// 催收区域 |
+            client_list :[],// 委托方 |
+            batch_list:[],// 批次 |
+            type_list:[],// |  | 案件类型 |
+            staff_id:[],
+            form:{
+                staff_id:"", 
+                area_id:"",
+                client_id:"",
+                paid:"",
+                batch_id:"",
+                type_id :""
+            },
+            tableData:[],
+            width:"width:170px"
         }
     },
     activated(){
-      this.init(0)
+      this.init()
+      this.initForm()
     },
     methods:{
-        index(val){
-        return  (this.form.page-1)*this.form.page_size+val+1
-      },
-      //数据列表
-      init(num){
-        const data = this.form
-        if(num == 0){
-          data.page = this.form.page
-          data.page_size = this.form.page_size
-        }else if(num == 1){
-          data.page = 1;
-          data.page_size = 10
-        }
-        const conf = {
-          url:"/api/api_backend.php?r=sms-log/sms-log",
-          data:data,
-          success:(data)=>{
-            if( data.statusCode == 1 ){
-              this.tableData = data.info.info
-              this.total = parseInt(data.info.total)
-              this.sms_count = parseInt(data.info.sms_count)
-            }else{
-              this.tableData = []
-              this.total = 0
-              this.sms_count = 0
+        initForm(){
+            const url = "/api/api_backend.php?r=collection-count/init"
+            const conf = {
+                url,
+                success:(data)=>{
+                    this.staff_list = data.info.staff_list
+                    this.area_list = data.info.area_list
+                    this.client_list = data.info.client_list
+                    this.batch_list = data.info.batch_list
+                    this.type_list = data.info.type_list
+                    console.log(data)
+                }
             }
-          }
-        }
-        axiosRequest(conf)
-      },
-         handleSizeChange(val) {
-        this.form.page_size = val
-        this.sendRecord(0)
-      },
-      handleCurrentChange(val) {
-        this.form.page = val
-        this.sendRecord(0)
-      },
+            axiosRequest(conf)
+        },
+        //数据列表
+        init(){
+            const data = this.form
+            data.staff_id = this.staff_id.join(",")
+            const conf = {
+            url:"/api/api_backend.php?r=collection-count/collection-list",
+            data:data,
+            success:(data)=>{
+                if( data.statusCode == 1 ){
+                    var arr = []
+                    for (let i in data.info) {
+                        arr.push(data.info[i])
+                    }
+                    this.tableData = arr
+                }else{
+                    this.tableData = []
+                }
+            }
+            }
+            axiosRequest(conf)
+        },
     }
 }
 </script>
