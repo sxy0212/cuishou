04cd5db1544f352afc1efb98bd7c681d350fcdd2
sxy <1043382831@qq.com>
2019-01-21 15:03:51 +0800

    账户余额

--- src/views/layout/components/Top.vue
+++ src/views/layout/components/Top.vue
@@ -3,13 +3,14 @@
 		<div class="header">
 			<div class="title"> <router-link to="/charts" style="color:#fff">言通言小信AI贷后管理软件</router-link></div>
 			<div class="drop">
-			<span @click="shuaxin" style="margin-right:20px;cursor:pointer;">刷新</span>
-			<span v-show="info.sys_login == 1"> <a href="/sysadmin.php" style="display:inline-block;color:#fff;margin-right:20px;cursor:pointer;"> 切回管理</a></span>
+			<span> 账户话费余额：<em class="red" style="font-weight: 800 ">{{money}}</em>元</span>
+			<span @click="shuaxin" style="margin-right:20px;cursor:pointer;margin-left:10px;">刷新</span>
+     	<span v-show="info.sys_login == 1"> <a href="/sysadmin.php" style="display:inline-block;color:#fff;margin-right:20px;cursor:pointer;"> 切回管理</a></span>
 			<el-dropdown trigger="hover" >
 				<span class="el-dropdown-link userinfo-inner"><img :src="this.sysUserAvatar" />{{info.company_name}}</span>
 				<el-dropdown-menu slot="dropdown" style="cursor: pointer;">
 					<el-dropdown-item>公司代号:{{info.prefix}}</el-dropdown-item>
-					<el-dropdown-item>设置</el-dropdown-item>
+					<!--<el-dropdown-item>设置</el-dropdown-item>-->
 					<el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
 				</el-dropdown-menu>
 			</el-dropdown>
@@ -31,6 +32,7 @@ import store from '@/vuex/store.js'
 					company_id:"",
 					company_name:""
 				},
+				money:"",
 				sysUserName: '',
 				sysUserAvatar: '',
 				form: {
@@ -53,10 +55,22 @@ import store from '@/vuex/store.js'
 				}
 			}
 			axiosRequest(conf)
+			this.moneySafe()
 		},
 		methods: {
 			onSubmit() {
 				console.log('submit!')
+			},
+			moneySafe(){
+				const url = "/api/api_backend.php?r=asrcall-bill/balance-info"
+				const conf = {
+					url,
+					success:(data)=>{
+						this.money = data.money
+						console.log(data)
+					}
+				}
+				axiosRequest(conf)
 			},
 			handleopen() {
 				//console.log('handleopen')
