6950fdc441d024564f51387b07b8a35a02d18bcb
sxy <1043382831@qq.com>
2019-02-19 10:32:29 +0800

    数据统计

--- src/views/nav31/dataStatistics.vue
+++ src/views/nav31/dataStatistics.vue
@@ -2,9 +2,12 @@
   <section>
 		<div style="margin-bottom:10px;">
       批次名称:
-      <el-select v-model="batch_id" placeholder="请选择" @change="changeBatch">
+      <el-select v-model="batch_id" placeholder="请选择">
         <el-option v-for="(item,index) in option" :label="item.batch_name" :value="item.id" :key="index"></el-option>
       </el-select>
+      选择时间:
+      <el-date-picker v-model="start_time" type="date" placeholder="选择日期" value-format="yyyy-MM-dd" style="width:200px"></el-date-picker>
+      <el-button @click="changeBatch" type="primary">生成图表</el-button>
     </div>
 		<div style="padding-bottom:10px;font-size:20px;display:flex;justify-content: space-between;">
       <div>
@@ -16,6 +19,14 @@
       </div>
     </div>
 		<div id="container" v-loading="loading"></div>
+     <el-table ref="multipleTable" :data="infos" style="width:100%" border v-show="showTable">
+          <el-table-column prop="time" label="日期" width="100"></el-table-column>  
+          <el-table-column prop="batch_name" label="所属批次"></el-table-column>  
+          <el-table-column prop="total" label="拨打量"></el-table-column>  
+          <el-table-column prop="conn" label="接通量"></el-table-column>  
+          <el-table-column prop="no_conn" label="未接通量"></el-table-column>  
+          <el-table-column prop="per_conn" label="接通率"></el-table-column>  
+				</el-table>
 	</section>
 </template>
 <script>
@@ -26,6 +37,9 @@ import {axiosRequest,message} from '@/assets/js/Yt.js'
 			return {
 				option:[],
 				batch_id:"",
+        start_time:"",
+        infos:[],
+        showTable:false,
         loading:true,
         dates:[{
                 name: "",
@@ -57,20 +71,32 @@ import {axiosRequest,message} from '@/assets/js/Yt.js'
 				}
 				axiosRequest(conf)
 			},
-      changeBatch(val){
-        this.batch_id = val
+      changeBatch(){
+        
         this.init()
+        if(this.start_time == '' || this.start_time == null){
+          this.showTable = false
+        }else{
+          this.showTable = true
+        }
       },
       init(){
         const url = "/api/api_backend.php?r=call-stat/data-show"
         const batch_id = this.batch_id
+        let start_time = ""
+        if(this.start_time == null){
+          start_time = ""
+        }else{
+          start_time = this.start_time
+        }
         const conf = {
           url,
-          data:{batch_id},
+          data:{batch_id,start_time},
           success:(data)=>{
-            this.tip.all = data.info.data.all;
-            this.tip.conn = data.info.data.conn;
-            this.tip.per_conn = data.info.data.per_conn
+            this.infos = data.info.table
+            this.tip.all = data.info.data[0].all;
+            this.tip.conn = data.info.data[0].conn;
+            this.tip.per_conn = data.info.data[0].per_conn
             this.x = data.info.chart.map( item=>item.time)
             this.dates[0].name = "拨打电话量"
             this.dates[0].data =  data.info.chart.map( item=>Number(item.num))
@@ -84,7 +110,13 @@ import {axiosRequest,message} from '@/assets/js/Yt.js'
         axiosRequest(conf)
       },
       exportData(){
-        window.open("/api/api_backend.php?r=call-stat/export&batch_id="+this.batch_id)
+        let start_time = ""
+        if(this.start_time == null){
+          start_time = ""
+        }else{
+          start_time = this.start_time
+        }
+        window.open("/api/api_backend.php?r=call-stat/export&batch_id="+this.batch_id+'&start_time='+start_time)
       },
 		  // 生成图表
       loadchart(num1,num2){
